3ee547edebda52cc1a133356996c01297b72f774
Sandra Eng <sandra.eng@live.no>
2023-10-11 13:39:10 +0200

    commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure, totam possimus deleniti facilis deserunt magnam aperiam facere praesentium nulla doloremque dolor voluptatibus. Quisquam expedita magni animi architecto possimus omnis doloribus aspernatur sunt quasi provident? Architecto suscipit ea, nisi sunt expedita laudantium modi omnis. Aliquam aspernatur, voluptate quibusdam inventore distinctio deleniti, asperiores animi provident vel, vitae magnam atque? Facilis sint dolor nesciunt fugit labore necessitatibus, quas exercitationem rerum repudiandae in ullam ea consectetur. Quas eum laboriosam libero aliquid magni? Rerum harum mollitia, non placeat praesentium sequi provident est quos ea numquam assumenda. Libero impedit fugit ullam facere porro quod voluptas?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure, totam possimus deleniti facilis deserunt magnam aperiam facere praesentium nulla doloremque dolor voluptatibus. Quisquam expedita magni animi architecto possimus omnis doloribus aspernatur sunt quasi provident? Architecto suscipit ea, nisi sunt expedita laudantium modi omnis. Aliquam aspernatur, voluptate quibusdam inventore distinctio deleniti, asperiores animi provident vel, vitae magnam atque? Facilis sint dolor nesciunt fugit labore necessitatibus, quas exercitationem rerum repudiandae in ullam ea consectetur. Quas eum laboriosam libero aliquid magni? Rerum harum mollitia, non placeat praesentium sequi provident est quos ea numquam assumenda. Libero impedit fugit ullam facere porro quod voluptas?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
